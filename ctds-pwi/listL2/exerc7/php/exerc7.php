ec02f6b00314d5a57311265ce51569d62c76c402
Vinicius Carpes de Freitas <147566186+vinicarpes@users.noreply.github.com>
2024-10-29 17:27:34 -0300

    iniciando exercício 7 da lista 2 de PHP

---
+++ ctds-pwi/listL2/exerc7/php/exerc7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/formata-pagina.css">
+    <title>Conversor de câmbio</title>
+</head>
+<body>
+
+    <h1>Resumo de compra</h1>
+
+    <?php
+        define("DESC_CARTAO", 0.05);
+        define("TAXA_ENTREGA",0.02);
+
+        $valIncial = $_POST["valor-compra"];
+        $tipoPagamento = ["tipo-pagamento"];
+        $entragaDom = ["entrega-domiciliar"];
+    
+    ?>
+</body>
+</html>
