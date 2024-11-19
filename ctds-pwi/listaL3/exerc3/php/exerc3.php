b981e8208b52207680e1e44eda11127d3cae2ae3
Vinicius Carpes de Freitas <147566186+vinicarpes@users.noreply.github.com>
2024-11-19 17:11:22 -0300

    meso exercicio que o 2, porem usando bubble sort

---
+++ ctds-pwi/listaL3/exerc3/php/exerc3.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/formata-pagina.css">
+    <title>Relatório de desempenho acadêmico</title>
+</head>
+<body>
+
+    <h1>Avaliação I - Programação Web I</h1>
+    <?php 
+        $aluno1 = $_POST['aluno1'];
+        $aluno2 = $_POST['aluno2'];
+        $aluno3 = $_POST['aluno3'];
+
+        $nota1 = $_POST['nota1'];
+        $nota3 = $_POST['nota3'];
+        $nota2 = $_POST['nota2'];
+
+       $vetorNotas = [$aluno1 => $nota1, $aluno2 => $nota2, $aluno3 => $nota3];
+
+       //ordenação de dados
+       //sort() - ordena e cria novos indices
+       //rsort() - ordena desc e cria novos indices
+       //asort() - ordena e mantem os indices chave => valor
+       //arsort() - ordena desc e mantem os indices chave => valor
+       //ksort() - ordena os indices e mantem as associacoes
+       //krsort() - ordena desc os indices e mantem as associacoes
+    
+       arsort($vetorNotas);
+        echo "<table>
+            <caption> Relação de dados acadêmicos dos alunos de CTDS/PRWI</caption>
+        
+            <tr>
+                <th>Nome do aluno</th>
+                <th>Nota de PRWI</th>
+            </tr>";
+            foreach($vetorNotas as $nomeAluno => $nota)
+            {   
+
+                $maiorNota= max($vetorNotas); //maior valor do vetor
+                $alunoDestaque = array_search($maiorNota, $vetorNotas); //
+                //fazendo a logica manuamente
+                // if($maiorNota<$nota){
+                //     $maiorNota = $nota;
+                //     $alunoDestaque = $nomeAluno;
+                // }
+                echo
+                "<tr>
+                    <td>$nomeAluno</td>
+                    <td>$vetorNotas[$nomeAluno]</td>
+                </tr>
+                ";
+            }
+
+        echo "</table>";
+        echo "<p>Aluno destaque: $alunoDestaque<br>
+                Nota destaque: $maiorNota</p>"
+    ?>
+</body>
+</html>
