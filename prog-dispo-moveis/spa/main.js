5ca62166ed949798114cf0b24f32114cfd918eae
vinicarpes <vinicdf000@gmail.com>
2025-06-02 08:05:27 -0300

    transformando em pwa

--- prog-dispo-moveis/spa/main.js
+++ prog-dispo-moveis/spa/main.js
@@ -1,3 +1,22 @@
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register("./service-worker.js");
+}
+
+let pedidoInstalacao;
+window.addEventListener('beforeinstallprompt', function(installPrompt) {
+    if(installPrompt){
+        $("#installAppBt").show();
+        pedidoInstalacao = installPrompt;
+    }
+});
+
+var pgAtual = "#home";
+function mostraPagina(pg) {
+    $(pgAtual).hide();
+    $(pg).show();
+    pgAtual = pg;
+}
+
 function navega(destino){
     let telas = document.getElementsByClassName('tela')
     Array.from(telas).forEach(element => {
@@ -6,4 +25,8 @@ function navega(destino){
     });
     document.getElementById(destino).classList.remove('collapse')
     document.getElementById(destino).classList.add('show')
+}
+
+function installApp() {
+    pedidoInstalacao.prompt(); 
 }
